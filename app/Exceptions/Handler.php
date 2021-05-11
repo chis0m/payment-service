fdba1e3b730af6368700da75b9312de28ef9d27d
chisom <chisom.e@credpal.com>
2021-05-11 02:31:40 +0100

    added response traits and exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,18 +2,29 @@
 
 namespace App\Exceptions;
 
+use App\Traits\TResponder;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use TResponder;
     /**
      * A list of the exception types that are not reported.
      *
      * @var array
      */
     protected $dontReport = [
-        //
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
     ];
 
     /**
@@ -32,10 +43,52 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
+    public function register() :void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+        public function render($request, Throwable $e)
+    {
+        if ($e instanceof PaymentException) {
+            $code = $e->getCode();
+            $message = $e->getMessage();
+            return $this->error(null, $message, $code);
+        }
+
+            // If Model Not found (e.g: not existing user error)
+        if ($e instanceof ModelNotFoundException) {
+            $model = ucwords(strtolower(class_basename($e->getModel())));
+            return $this->error(
+                null,
+                "Does not exist any instance of {$model} with the given id",
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        // Handling the Unauthorized exception
+        if ($e instanceof AuthorizationException) {
+            return $this->error(null, $e->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+
+        if ($e instanceof AuthenticationException) {
+            return $this->error(null, $e->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($e instanceof ValidationException) {
+            $errors = $e->validator->errors()->getMessages();
+            return $this->error($errors, 'invalid credentials', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $e);
+        }
+
+        // @phpstan-ignore-next-line
+        return $this->fatalError($e);
     }
 }
